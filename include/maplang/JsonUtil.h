b20910024341827e470671019432197ff68a21ee
Rick Kern <kernrj@gmail.com>
2020-10-24 09:47:19 -0400

    Add JsonUtil for nlohmann::json convenience functions.

---
+++ include/maplang/JsonUtil.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2020 VDO Dev Inc <support@maplang.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef MAPLANG__JSONUTIL_H_
+#define MAPLANG__JSONUTIL_H_
+
+#include <string>
+#include "json.hpp"
+
+namespace maplang {
+
+template <class T>
+T jsonGetOr(const nlohmann::json& j, const std::string& key, T orValue) {
+  if (j.contains(key)) {
+    return j[key].get<T>();
+  } else {
+    return orValue;
+  }
+}
+
+template <class T>
+bool jsonTryGet(const nlohmann::json& j, const std::string& key, T* valueOut) {
+  if (!j.contains(key)) {
+    return false;
+  }
+
+  *valueOut = j[key].get<T>();
+  return true;
+}
+
+}  // namespace maplang
+
+#endif  // MAPLANG__JSONUTIL_H_
